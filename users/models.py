bebb42d21cb452be68755a29e8da25a08c22086d
Aleksey40000 <alekseyfilippov9392@gmail.com>
2026-02-27 01:42:35 +0700

    Пример подключения к postgress

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
 
 
@@ -12,3 +13,61 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+class Payment(models.Model):
+    CASH = "cash"
+    TRANSFER = "transfer"
+
+    PAYMENT_METHODS = [
+        (CASH, "Наличные"),
+        (TRANSFER, "Перевод на счет"),
+    ]
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="payments",
+        verbose_name="Пользователь"
+    )
+
+    payment_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата оплаты"
+    )
+
+    paid_course = models.ForeignKey(
+        "courses_and_training.Course",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="course_payments",
+        verbose_name="Оплаченный курс"
+    )
+
+    paid_lesson = models.ForeignKey(
+        "courses_and_training.Lesson",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="lesson_payments",
+        verbose_name="Оплаченный урок"
+    )
+
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Сумма оплаты"
+    )
+
+    payment_method = models.CharField(
+        max_length=20,
+        choices=PAYMENT_METHODS,
+        verbose_name="Способ оплаты"
+    )
+
+    def __str__(self):
+        return f"{self.user} - {self.amount}"
