7f815f1cf79ebfa623dc9b69751909a5a3e6a81f
Aleksey40000 <alekseyfilippov9392@gmail.com>
2026-03-13 01:07:02 +0700

    Работа над замечаниями тесты

--- courses_and_training/tests.py
+++ courses_and_training/tests.py
@@ -1,22 +1,53 @@
 from rest_framework.test import APITestCase
-from django.contrib.auth.models import User
-from .models import Course
+from users.models import User
+from .models import Course, Subscription
+from rest_framework import status
 
 
 class CourseTestCase(APITestCase):
 
     def setUp(self):
-
-        self.user = User.objects.create(username="test_user")
+        self.user = User.objects.create(
+            email="test@test.com",
+            password="12345"
+        )
 
         self.client.force_authenticate(user=self.user)
 
-        self.course = Course.objects.create(title="Test Course", owner=self.user)
+        self.course = Course.objects.create(
+            title="Test Course",
+            description="Test",
+            owner=self.user
+        )
 
     def test_create_course(self):
+        data = {
+            "title": "New Course",
+            "description": "Test Course"
+        }
 
-        data = {"title": "New Course", "description": "Test"}
+        response = self.client.post("/api/courses/", data)
 
-        response = self.client.post("/courses/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        self.assertEqual(response.status_code, 201)
+
+    def test_course_list(self):
+        response = self.client.get("/api/courses/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_subscription(self):
+        data = {
+            "course_id": self.course.id
+        }
+
+        response = self.client.post("/api/subscribe/", data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertTrue(
+            Subscription.objects.filter(
+                user=self.user,
+                course=self.course
+            ).exists()
+        )
